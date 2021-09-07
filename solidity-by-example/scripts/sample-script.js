54b28ad91b2da3fea273d94d83c2d7e5931ca7f2
ihm <s5935512030@phuket.psu.ac.th>
2021-09-07 21:19:19 +0700

    test  struct

--- solidity-by-example/scripts/sample-script.js
+++ solidity-by-example/scripts/sample-script.js
@@ -17,19 +17,38 @@ async function main() {
 
   console.log("Array deployed to:", array.address);
 
-  console.log("array: ",await array.get())
+  console.log("array: ", await array.get())
   array.add(1)
   array.add(2)
   array.add(3)
   array.add(4)
   array.add(5)
-  console.log("array: ",await array.get())
+  console.log("array: ", await array.get())
   array.remove(3)
-  console.log("array: ",await array.get())
+  console.log("array: ", await array.get())
   array.add(3)
   array.removeNoneGap(3)
-  console.log("array: ",await array.get())
-  console.log("count: ",await array.count())
+  console.log("array: ", await array.get())
+  console.log("count: ", await array.count())
+
+
+  const Todos = await hre.ethers.getContractFactory("Todos");
+  const todos = await Todos.deploy();
+
+  console.log("Todos deployed to:", todos.address);
+  todos.create("Learn solidity")
+  console.log("todos: ", await todos.get(0))
+  todos.update(0,"solidity")
+  console.log("todos: ", await todos.get(0))
+  todos.toggleStatus(0)
+  console.log("todos: ", await todos.get(0))
+
+
+
+
+
+  await todos.deployed();
+
 }
 
 
