89b6fd70b652fb396ce9387cfe79f3f7a38f586f
Ismael (Air) <ismael@scb10x.com>
2021-09-19 22:00:15 +0700

    add constructor

--- solidity-by-example/contracts/Cunstructor.sol
+++ solidity-by-example/contracts/Cunstructor.sol
@@ -2,5 +2,28 @@
 
 pragma solidity ^0.8.4;
 
-contract Skip {}
-//skip
+contract HiNum {
+    uint256 public num;
+
+    constructor(uint256 _num) {
+        num = _num;
+    }
+}
+
+contract HiAir {
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
+
+contract Hi is HiNum(1), HiAir("Hi AIR") {}
+
+contract Hi2 is HiNum, HiAir {
+    constructor() HiNum(1) HiAir("Hi AIR") {}
+}
+
+contract Hi3 is HiNum, HiAir {
+    constructor() HiAir("Hi AIR") HiNum(1) {}
+}
