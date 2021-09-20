385b7abd97f5c239f8d06c5ee9213eb9dc077b42
Ismael (Air) <ismael@scb10x.com>
2021-09-20 09:14:02 +0700

    update counter using interface

--- solidity-by-example/contracts/Counter.sol
+++ solidity-by-example/contracts/Counter.sol
@@ -3,17 +3,41 @@
 pragma solidity ^0.8.4;
 
 contract Counter {
-    uint256 public counter;
+    uint256 public count;
 
-    function getCount() public view returns (uint256) {
-        return counter;
+    function increment() external {
+        count += 1;
     }
 
-    function add() public {
-        counter += 1;
+    function decrement() external {
+        count -= 1;
+    }
+}
+
+interface ICounter {
+    function count() external view returns (uint256);
+
+    function increment() external;
+
+    function decrement() external;
+}
+
+contract MyContract {
+    ICounter count;
+
+    constructor(address _counterAddress) {
+        count = ICounter(_counterAddress);
     }
 
-    function dec() public {
-        counter -= 1;
+    function incrementCounter() external {
+        count.increment();
+    }
+
+    function decrementCounter() external {
+        count.decrement();
+    }
+
+    function getCount() external view returns (uint256) {
+        return count.count();
     }
 }
